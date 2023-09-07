053c22aab61625d039e33dbbd00bc79c6896c708
ghcruise <ghcruise@hotmail.com>
2023-09-07 14:45:22 +0800

    update huijiwiki translations

--- function/testNode.py
+++ function/testNode.py
@@ -4,7 +4,7 @@ from translate_uni2zh import uni2zh
 # str = "利益外缘 (冥王星比邻星域)"
 # node=translateNode(str)
 # print(node)
-snd = "CrewBattleNode519"
-snode = solNode(snd)
-print(snode)
-print(uni2zh("/Lotus/Powersuits/Archwing/ArchwingAbilities/FlareCounterMeasureAbility"))
+# snd = "CrewBattleNode519"
+# snode = solNode(snd)
+print(solNode("SolNode196"))
+print(uni2zh("/Lotus/Weapons/Corpus/Melee/KickAndPunch/KickPunchWeapon"))
---
+++ translate/get_wiki_translations.py
@@ -0,0 +1,24 @@
+import urllib
+from urllib.request import urlretrieve
+import json
+def recu_down(url,filename): # recurrent download with ContentTooShortError
+    try:
+        urlretrieve(url,filename)
+    except urllib.error.ContentTooShortError:
+        print ('Network conditions is not good. Reloading...')
+        recu_down(url,filename)
+
+recu_down("https://warframe.huijiwiki.com/api.php?action=query&format=json&prop=revisions&titles=Data%3AUserDict.json&formatversion=2&rvprop=content&rvlimit=1","translate/wiki_translations.json")
+
+with open("translate/wiki_translations.json",'r',encoding='utf-8')as f:
+    content = json.load(f)
+    content = content['query']['pages'][0]['revisions'][0]['content']
+with open("translate/wiki_translations.json",'w',encoding='utf-8')as f:
+    f.writelines(content)
+del content
+
+with open("translate/wiki_translations.json",'r',encoding='utf-8')as f:
+    content = f.readlines()
+content = [line.replace('",','",\n') for line in content[:]]
+with open("translate/wiki_translations.json",'w',encoding='utf-8')as f:
+    f.writelines(content)
