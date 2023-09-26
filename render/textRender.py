10eb75733175598a48c92823da60092eab2fb19d
ghcruise <ghcruise@hotmail.com>
2023-09-26 11:38:30 +0800

    WIP

--- render/textRender.py
+++ render/textRender.py
@@ -1,15 +1,14 @@
 from PIL import Image,ImageDraw,ImageFont,ImageFilter
 import requests
-text="text\ntest\n测试"
  
 api=requests.get("http://nymph.rbq.life:3000/wm/robot/摸")
 text=api.text
 
 # PIL实现
-width=100*8
-height=100*4
+width=616
+height=610
 im=Image.new('RGB',(width,height),(255,255,255))
 dr=ImageDraw.Draw(im)
-font=ImageFont.truetype("render/msyh.ttc",20)
-dr.text((10,5),text,font=font,fill='#000000')
+font=ImageFont.truetype("render/msyh.ttc",25)
+dr.text((5,5),text,font=font,fill='#0000FF')
 im.save("render/t.png")
