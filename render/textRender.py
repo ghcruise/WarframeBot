de87061514bb157ab4e7fc05df7d3b3618b7e2e1
ghcruise <ghcruise@hotmail.com>
2023-09-26 08:56:01 +0800

    WIP

--- render/textRender.py
+++ render/textRender.py
@@ -1,11 +1,13 @@
 from PIL import Image,ImageDraw,ImageFont,ImageFilter
- 
+import requests
 text="text\ntest\n测试"
  
- 
+api=requests.get("http://nymph.rbq.life:3000/wm/robot/摸")
+text=api.text
+
 # PIL实现
-width=60*4
-height=60*2
+width=100*8
+height=100*4
 im=Image.new('RGB',(width,height),(255,255,255))
 dr=ImageDraw.Draw(im)
 font=ImageFont.truetype("render/msyh.ttc",20)
