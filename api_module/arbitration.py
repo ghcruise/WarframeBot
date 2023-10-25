11fc88f26fe5ec0927b7593ed9b4b7afa53e6940
ghcruise <ghcruise@hotmail.com>
2023-10-25 10:36:38 +0800

    update

--- api_module/arbitration.py
+++ api_module/arbitration.py
@@ -25,50 +25,50 @@ def arbitration():
     
     try:
         arbitrationStats = requests.get("https://10o.io/arbitrations.json",timeout=(5,5))
-        arbitration_dict = arbitrationStats.json()
-        print("[ init ] Arbitration. Status code:",arbitrationStats.status_code)
-        solnode_info = solNode(arbitration_dict[0]['solnode'])
-        try:
-            _ = pushArbitration.index(arbitration_dict[0]['solnode'])
-            push = 1
-        except:
-            push = 0
-        #获取并翻译节点位置
-        if (arbitration_dict[0]['solnode'] == "SolNode450"):
-            arbitrationContent = "Tyana Pass 火星 - Corpus\n - 镜像防御"
-        else:
-            arbitrationContent =  "仲裁更新了:\n\n"\
-        + solnode_info['name'] + " "\
-        + solnode_info['systemName'] + " - "\
-        + solnode_info['faction'] + "\n - "\
-        + solnode_info['type']
-        #处理卡片消息
-        arbitrationRefreshTime = get_time_stamp(arbitration_dict[0]['end']) * 1000
-        arbitrationCard = {
-            "type": "card",
-            "theme": "success",
-            "size": "lg",
-            "modules": [
-            {
-                "type": "section",
-                "text": {
-                "type": "plain-text",
-                "content": arbitrationContent
-                }
-            },
-            {
-            "type": "countdown",
-            "mode": "day",
-            "endTime": arbitrationRefreshTime
-            }
-            ]
-        }
-        #print(arbitrationCard)
-        #响应并回复
-        cm = CardMessage(arbitrationCard)
-        return cm,push
     except:
         print("retry...")
         arbitration()
+    arbitration_dict = arbitrationStats.json()
+    print("[ init ] Arbitration. Status code:",arbitrationStats.status_code)
+    solnode_info = solNode(arbitration_dict[0]['solnode'])
+    try:
+        _ = pushArbitration.index(arbitration_dict[0]['solnode'])
+        push = 1
+    except:
+        push = 0
+    #获取并翻译节点位置
+    if (arbitration_dict[0]['solnode'] == "SolNode450"):
+        arbitrationContent = "Tyana Pass 火星 - Corpus\n - 镜像防御"
+    else:
+        arbitrationContent =  "仲裁更新了:\n\n"\
+    + solnode_info['name'] + " "\
+    + solnode_info['systemName'] + " - "\
+    + solnode_info['faction'] + "\n - "\
+    + solnode_info['type']
+    #处理卡片消息
+    arbitrationRefreshTime = get_time_stamp(arbitration_dict[0]['end']) * 1000
+    arbitrationCard = {
+        "type": "card",
+        "theme": "success",
+        "size": "lg",
+        "modules": [
+        {
+            "type": "section",
+            "text": {
+            "type": "plain-text",
+            "content": arbitrationContent
+            }
+        },
+        {
+        "type": "countdown",
+        "mode": "day",
+        "endTime": arbitrationRefreshTime
+        }
+        ]
+    }
+    #print(arbitrationCard)
+    #响应并回复
+    cm = CardMessage(arbitrationCard)
+    return cm,push
 # print(arbitration()[0][0]['theme'])
-# print(arbitration()[1])
+# print(arbitration())
