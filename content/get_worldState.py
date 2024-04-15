11cf6487acfd4f79776489019e3cb11b8ef20291
ghcruise <ghcruise@hotmail.com>
2024-04-15 17:40:31 +0800

    update

--- content/get_worldState.py
+++ content/get_worldState.py
@@ -1,6 +1,7 @@
 import socket
 import urllib
 import datetime
+import json
 from urllib.request import urlretrieve
 
 socket.setdefaulttimeout(10)
@@ -28,7 +29,8 @@ def getWorldState():
     recu_down("http://content.warframe.com/dynamic/worldState.php", "content/worldState.json")
     with open("content/worldState.json",'r', encoding='utf-8')as f1:
         content = f1.readlines()
-    # content= [line.replace('},',"},\n").replace('],','],\n') for line in content[:]]
+    #content= [line.replace('},',"},\n").replace('],','],\n') for line in content[:]]
+    #content= [line.replace('},',"},\n") for line in content[:]]
     with open("content/worldState.json",'w', encoding='utf-8')as f2:
         f2.writelines(content)
     print(f"{datetime.datetime.now()} [World] WorldState updated.")
